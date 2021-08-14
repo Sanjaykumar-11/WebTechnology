8dde63d89a70e7d1cb6b317119a6ba6efd157f8e
Sanjaykumar-11 <sanjay484641@gmail.com>
2021-08-14 16:19:47 +0530

    completed frontend

--- script.js
+++ script.js
@@ -51,34 +51,35 @@ function addTransactionDOM(transaction)
   list.appendChild(item);
 }
 
-function updateValues() {
+function updateValues() 
+{
   const amounts = transactions.map((transaction) => transaction.amount);
   const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
   const income = amounts
     .filter((item) => item > 0)
     .reduce((acc, item) => (acc += item), 0)
     .toFixed(2);
-  const expense = (
-    amounts.filter((item) => item < 0).reduce((acc, item) => (acc += item), 0) *
-    -1
-  ).toFixed(2);
+  const expense = (amounts.filter((item) => item < 0).reduce((acc, item) => (acc += item), 0) * -1).toFixed(2);
 
   balance.innerText = `${total}`;
   money_plus.innerText = `${income}`;
   money_minus.innerText = `${expense}`;
 }
 
-function removeTransaction(id) {
+function removeTransaction(id) 
+{
   transactions = transactions.filter((transaction) => transaction.id !== id);
   updateLocalStorage();
   init();
 }
 
-function updateLocalStorage() {
+function updateLocalStorage() 
+{
   localStorage.setItem("transactions", JSON.stringify(transactions));
 }
 
-function init() {
+function init() 
+{
   list.innerHTML = "";
   transactions.forEach(addTransactionDOM);
   updateValues();
